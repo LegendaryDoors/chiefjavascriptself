ac1567a0e07f88bfceff493345ba060f997ce137
legendarydoors <chris42004@hotmail.com>
2022-07-01 02:54:18 -0400

    owner only wtf oops

--- commands/image/innocent.js
+++ commands/image/innocent.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "innocent",
   catagory: "image",
   permissions: [],
-  devOnly: true,
+  devOnly: false,
   run: async ({ client, message, args }) => {
     message.channel.send({ files: [`media/chief/innocent.jpg`], content: "Chief is Innocent :100:"});
   },
